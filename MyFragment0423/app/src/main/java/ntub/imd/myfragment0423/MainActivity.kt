c66e9992231bab4da9145cbf1fc0d681c2d56d61
leonjyentub <63442573+leonjyentub@users.noreply.github.com>
2025-04-24 17:46:14 +0800

    :D nothing

--- MyFragment0423/app/src/main/java/ntub/imd/myfragment0423/MainActivity.kt
+++ MyFragment0423/app/src/main/java/ntub/imd/myfragment0423/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class ViewPagerAdapter(activity: MainActivity): FragmentStateAdapter(activity){
     override fun getItemCount() = 3
@@ -21,6 +22,7 @@ class ViewPagerAdapter(activity: MainActivity): FragmentStateAdapter(activity){
     }
 }
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewpager: ViewPager2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -30,10 +32,51 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val viewpapger = findViewById<ViewPager2>(R.id.viewpager)
-        viewpapger.adapter = ViewPagerAdapter(this)
-        findViewById<Button>(R.id.btnFirst).setOnClickListener { viewpapger.setCurrentItem(0, true) }
-        findViewById<Button>(R.id.btnSecond).setOnClickListener { viewpapger.setCurrentItem(1, true) }
-        findViewById<Button>(R.id.btnThird).setOnClickListener { viewpapger.setCurrentItem(2, true) }
+        viewpager = findViewById<ViewPager2>(R.id.viewpager)
+        viewpager.adapter = ViewPagerAdapter(this)
+        viewpager.setPageTransformer(Pager2_CubeInDepthTransformer())
+        //findViewById<Button>(R.id.btnFirst).setOnClickListener { viewpager.setCurrentItem(0, true) }
+        //findViewById<Button>(R.id.btnSecond).setOnClickListener { viewpager.setCurrentItem(1, true) }
+        //findViewById<Button>(R.id.btnThird).setOnClickListener { viewpager.setCurrentItem(2, true) }
+        val btmNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        viewpager.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                btmNav.selectedItemId = when(position){
+                    0 -> R.id.btnLeft
+                    1 -> R.id.btnMiddle
+                    2 -> R.id.btnRight
+                    else -> R.id.btnLeft
+                }
+            }
+        })
+        btmNav.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.btnLeft -> {
+                    viewpager.currentItem = 0
+                    true
+                }
+                R.id.btnMiddle -> {
+                    viewpager.currentItem = 1
+                    true
+                }
+                R.id.btnRight -> {
+                    viewpager.currentItem = 2
+                    true
+                }
+                else-> {
+                    viewpager.currentItem = 0
+                    true
+                }
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        if(viewpager.currentItem > 0){
+            viewpager.currentItem = viewpager.currentItem -1
+        }else{
+            super.onBackPressed()
+        }
     }
 }
