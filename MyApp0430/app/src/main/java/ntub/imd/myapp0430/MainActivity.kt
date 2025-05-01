079b7dd25cf94de0d9b5994adc5a19dabe3adb9e
leonjyentub <63442573+leonjyentub@users.noreply.github.com>
2025-05-01 16:38:37 +0800

    threads

---
+++ MyApp0430/app/src/main/java/ntub/imd/myapp0430/MainActivity.kt
@@ -0,0 +1,68 @@
+package ntub.imd.myapp0430
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btnStart = findViewById<Button>(R.id.btnStart)
+        val sbarBunny = findViewById<SeekBar>(R.id.sbarBunny)
+        val sbarTurtle = findViewById<SeekBar>(R.id.sbarTurtle)
+        val txtResult = findViewById<TextView>(R.id.txtResult)
+        btnStart.setOnClickListener {
+            btnStart.isEnabled = false
+            sbarTurtle.progress  = 0
+            sbarBunny.progress = 0
+            val MAX = 100
+            var progressBunny = 0
+            var progressTurtle = 0
+            Thread{
+                while(progressBunny < MAX && progressTurtle < MAX){
+                    Thread.sleep(100)
+                    progressTurtle += 1
+                    sbarTurtle.progress = progressTurtle
+                }
+                Log.d("racing", "onClickUsingThread-turtle: end")
+                runOnUiThread {
+                    if(progressTurtle >= MAX && progressBunny < MAX){
+                        txtResult.text = "烏龜贏了！"
+                    }
+                }
+            }.start()
+
+            Thread {
+                val sleepProb = arrayOf(true, true, false)
+                while (progressBunny < MAX && progressTurtle < MAX) {
+                    Thread.sleep(100)
+                    if (sleepProb.random())
+                        Thread.sleep(200) //假設免子會多睡一點
+                    progressBunny += 3
+                    sbarBunny.progress = progressBunny
+
+                    Log.d("racing", "onClickUsingThread-bunny: end")
+                    runOnUiThread {
+                        if (progressBunny >= MAX && progressTurtle < MAX) {
+                            txtResult.text = "免子贏了！"
+                        }
+                        btnStart.isEnabled = true
+                    }
+                }
+            }.start()
+        }
+    }
+}
