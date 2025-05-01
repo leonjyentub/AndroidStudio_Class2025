6e7efe6f262de8d22035ec04e885f0e36762ce99
leonjyentub <63442573+leonjyentub@users.noreply.github.com>
2025-05-01 17:00:28 +0800

    add handler

--- MyApp0430/app/src/main/java/ntub/imd/myapp0430/MainActivity.kt
+++ MyApp0430/app/src/main/java/ntub/imd/myapp0430/MainActivity.kt
@@ -1,6 +1,8 @@
 package ntub.imd.myapp0430
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.widget.Button
 import android.widget.SeekBar
@@ -24,14 +26,17 @@ class MainActivity : AppCompatActivity() {
         val sbarBunny = findViewById<SeekBar>(R.id.sbarBunny)
         val sbarTurtle = findViewById<SeekBar>(R.id.sbarTurtle)
         val txtResult = findViewById<TextView>(R.id.txtResult)
+        val MAX = 100
+        var progressBunny = 0
+        var progressTurtle = 0
         btnStart.setOnClickListener {
             btnStart.isEnabled = false
             sbarTurtle.progress  = 0
             sbarBunny.progress = 0
-            val MAX = 100
-            var progressBunny = 0
-            var progressTurtle = 0
+            progressBunny = 0
+            progressTurtle = 0
             Thread{
+                //txtResult.text = "比賽進行！"
                 while(progressBunny < MAX && progressTurtle < MAX){
                     Thread.sleep(100)
                     progressTurtle += 1
@@ -41,6 +46,7 @@ class MainActivity : AppCompatActivity() {
                 runOnUiThread {
                     if(progressTurtle >= MAX && progressBunny < MAX){
                         txtResult.text = "烏龜贏了！"
+                        btnStart.isEnabled = true
                     }
                 }
             }.start()
@@ -50,7 +56,7 @@ class MainActivity : AppCompatActivity() {
                 while (progressBunny < MAX && progressTurtle < MAX) {
                     Thread.sleep(100)
                     if (sleepProb.random())
-                        Thread.sleep(200) //假設免子會多睡一點
+                        Thread.sleep(300) //假設免子會多睡一點
                     progressBunny += 3
                     sbarBunny.progress = progressBunny
 
@@ -58,9 +64,46 @@ class MainActivity : AppCompatActivity() {
                     runOnUiThread {
                         if (progressBunny >= MAX && progressTurtle < MAX) {
                             txtResult.text = "免子贏了！"
+                            btnStart.isEnabled = true
                         }
-                        btnStart.isEnabled = true
                     }
+                }
+            }.start()
+        }
+
+        val handler = Handler(Looper.getMainLooper()){ msg->
+            Log.d("racing", "onClickUsingHandler: msg: ${msg.what}")
+            if(progressBunny >= MAX && progressTurtle < MAX){
+                txtResult.text = "免子贏了！"
+            }else if(progressTurtle >= MAX && progressBunny < MAX){
+                txtResult.text = "烏龜贏了！"
+            }
+            btnStart.isEnabled = true
+            return@Handler true
+        }
+
+        val btnStart2 = findViewById<Button>(R.id.btnStart2)
+        btnStart2.setOnClickListener{
+            Thread{
+                while(progressBunny < MAX && progressTurtle < MAX){
+                    Thread.sleep(100)
+                    progressTurtle += 1
+                    sbarTurtle.progress = progressTurtle
+                }
+                handler.sendEmptyMessage(0)
+                Log.d("racing", "onClickUsingThread-handler-turtle: end")
+            }.start()
+
+            Thread {
+                val sleepProb = arrayOf(true, true, false)
+                while (progressBunny < MAX && progressTurtle < MAX) {
+                    Thread.sleep(100)
+                    if (sleepProb.random())
+                        Thread.sleep(300) //假設免子會多睡一點
+                    progressBunny += 3
+                    sbarBunny.progress = progressBunny
+                    handler.sendEmptyMessage(0)
+                    Log.d("racing", "onClickUsingThread-handler-bunny: end")
                 }
             }.start()
         }
