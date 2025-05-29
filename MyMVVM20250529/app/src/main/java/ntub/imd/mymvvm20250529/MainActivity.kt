e44b8302fbbfccf46f2d2cc27191c2b77f3ea59e
leonjyentub <63442573+leonjyentub@users.noreply.github.com>
2025-05-29 17:50:57 +0800

    MVVM from google-developer-training project
    https://github.com/google-developer-training/android-basics-kotlin-bus-schedule-app/

---
+++ MyMVVM20250529/app/src/main/java/ntub/imd/mymvvm20250529/MainActivity.kt
@@ -0,0 +1,31 @@
+package ntub.imd.mymvvm20250529
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import ntub.imd.mymvvm20250529.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragmentContainerView3) as NavHostFragment
+        navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
+    }
+}
---
+++ MyMVVM20250529/app/src/main/java/ntub/imd/mymvvm20250529/ui/dashboard/DashboardFragment.kt
@@ -0,0 +1,42 @@
+package ntub.imd.mymvvm20250529.ui.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import ntub.imd.mymvvm20250529.databinding.FragmentFullScheduleBinding
+
+class DashboardFragment : Fragment() {
+
+    private var _binding: FragmentFullScheduleBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val dashboardViewModel =
+            ViewModelProvider(this).get(DashboardViewModel::class.java)
+
+        _binding = FragmentFullScheduleBinding.inflate(inflater, container, false)
+
+        val root: View = binding.root
+        //val textView: TextView = binding.textDashboard
+        //dashboardViewModel.text.observe(viewLifecycleOwner) {
+        //    textView.text = it
+        //}
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        //_binding = null
+    }
+}
